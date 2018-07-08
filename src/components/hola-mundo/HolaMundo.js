7c8c3d7a93c26ecaa8a08cc7b1ec4285ecbabdd5
IsmaelJdz <ismael_br7@hotmail.com>
2018-07-07 21:15:45 -0500

    Creacion de componente Liga y calendario asi como el uso de react router dom

--- src/components/hola-mundo/HolaMundo.js
+++ src/components/hola-mundo/HolaMundo.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Button from 'material-ui/Button';
+
+import './HolaMundo.css';
 class HolaMundo extends React.Component {
     render(){
         return (
@@ -6,6 +9,8 @@ class HolaMundo extends React.Component {
                 {/*Comentario en jsx*/}
                 <p>Hola mundo! {this.props.nombre}</p>
                 <p>Hola mundo2!</p>
+                <p className="desc">Hola como estan esta es una pequeña descripcion de mi</p>
+                <Button variant="raised" color="primary">Enviar</Button>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import { render } from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'; 
 
-import HolaMundo from './components/HolaMundo'; 
+// Material
+import CssBaseline from 'material-ui/CssBaseline';
 
-render(<HolaMundo nombre="Ismael" edad="31"/>, document.querySelector('#root')); 
+// Estilos
+import './index.css';
+
+// Componentes
+import Header from './components/common/header/Header';
+// import Liga from './components/liga/Liga';
+// import Calendario from './components/calendario/Calendario';
+import Router from "./Router";
+
+
+// The CODE!
+const Root = () => {
+    return (
+        <div>
+            <CssBaseline />
+            <Header />
+            <div className="contenedor">
+                {/* <Liga />
+                <Calendario /> */}
+                <Router />
+            </div>
+        </div>
+    )
+}
+
+render(<Root />, document.querySelector('#root'));
 
 registerServiceWorker();
